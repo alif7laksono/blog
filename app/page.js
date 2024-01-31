9e23b127a6b66dc51fcfe7aacd21de89b1e99f47
Alif Laksono <aliflaksono@Alifs-MacBook-Air.local>
2024-01-31 10:25:58 +0700

    first commit

---
+++ app/page.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Home() {
+  return (
+    <div>
+      <h1 className='underline text-3xl text-center'>Welcome to blog app</h1>
+    </div>
+  )
+}
